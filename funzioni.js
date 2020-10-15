b8220f097b8e4140364e8968e1d1af3e93d2056d
castel98 <chiaracastelnovo@yahoo.it>
2020-10-15 16:16:54 +0200

    throw ot js functions v.2

--- funzioni.js
+++ funzioni.js
@@ -3,3 +3,16 @@ function registra(){
       var pasRegi = document.getElementById("pasR").value;
       document.write(NomeRegi, pasRegi);
     }
+Lprodotti = new Array;
+function Prodottieprezzi(){
+      prodotti = new Array;
+      var prodotto = document.getElementById("lampone").value;
+      prodotti.push(prodotto);
+      document.write(prodotti)
+    }
+function provacheck(){
+        document.write(Lprodotti);
+    }
+function aggiungi(SingoloProd){
+     Lprodotti.push(SingoloProd);
+    }
