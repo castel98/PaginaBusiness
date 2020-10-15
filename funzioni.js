287ddef2c9df88f6bf80ef79e52cb5ac264462e4
castel98 <chiaracastelnovo@yahoo.it>
2020-10-15 16:11:44 +0200

    throw out js functions v.1

---
+++ funzioni.js
@@ -0,0 +1,5 @@
+function registra(){
+      var NomeRegi = document.getElementById("NomR").value;
+      var pasRegi = document.getElementById("pasR").value;
+      document.write(NomeRegi, pasRegi);
+    }
