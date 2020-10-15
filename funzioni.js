4e4d4bfd1487a8d81ef95265fe5d1e8b6cb76633
castel98 <chiaracastelnovo@yahoo.it>
2020-10-15 16:20:31 +0200

    throw ot js functions v.3

--- funzioni.js
+++ funzioni.js
@@ -16,3 +16,58 @@ function provacheck(){
 function aggiungi(SingoloProd){
      Lprodotti.push(SingoloProd);
     }
+function verificanome()
+    {
+
+      var NomeArr = [ {name : 'chiara', password : 'Ciaretta28.'}, {name: 'laura', password : 'lalli03'}];
+      if (document.getElementById("txtname".value==""))
+      {
+        alert("Inserire un nome valido");
+
+        document.getElementById("txtname").focus();
+
+        return(false);
+      }
+      else {
+        var exist = false;
+        for (var i = 0; i <NomeArr.length; i++) {
+        if (NomeArr[i].name== document.getElementById("txtname").value) {
+          exist = true;
+          verifica(NomeArr[i].password)
+        }
+        }
+        if (exist == false)
+        {
+          document.write("nome utente inesistente");
+        }
+      }
+
+    }
+function verifica(perspas)
+    {
+
+      if (document.getElementById("txtpassword").value=="")
+
+    {
+
+     alert("Inserire una password valida");
+
+     document.getElementById("txtpassword").focus();
+
+     return(false);
+
+    }
+    else {
+      var check = false;
+
+      if (perspas.toString() == document.getElementById("txtpassword").value)
+      {
+          self.location.href='/home/chiara/Scrivania/PaginaBusiness/pagina2.html'
+      }
+      else {
+        document.write("password errata");
+      }
+
+      }
+
+    }
